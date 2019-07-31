1413a601c80517c6dde11deeb629403784bbf98c
Michael Warner <mwarner1040@gmail.com>
2019-07-31 10:10:56 -0400

    Removes commented portion from upload_file.js

--- upload_file.js
+++ upload_file.js
@@ -1,4 +1,4 @@
-// Load the SDK and UUID
+// File imported into server.js
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
 var multer = require('multer');
@@ -36,30 +36,4 @@ const upload = multer({
 
 module.exports = upload;
 
-
-
-// // Create unique bucket name
-// var bucketName = 'node-sdk-sample-' + uuid.v4();
-// // var bucketName = 'disinterest-graphql-project-dev-' + uuid.v4();
-// // Create name for uploaded object key
-// var keyName = 'hello_world.txt';
-
-// // Create a promise on S3 service object
-// var bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' }).createBucket({ Bucket: bucketName }).promise();
-
-// // Handle promise fulfilled/rejected states
-// bucketPromise.then(
-//   function (data) {
-//     // Create params for putObject call
-//     var objectParams = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
-//     // Create object upload promise
-//     var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' }).putObject(objectParams).promise();
-//     uploadPromise.then(
-//       function (data) {
-//         console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-//       });
-//   }).catch(
-//     function (err) {
-//       console.error(err, err.stack);
-//     });
 
