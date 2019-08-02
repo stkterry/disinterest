4a9db44bd2a4bff36949782d8a854495c1d6b576
Michael Warner <mwarner1040@gmail.com>
2019-08-02 17:02:07 -0400

    Modifies loadFromPath within upload_file.js to be specific to my development environment

--- server/upload_file.js
+++ server/upload_file.js
@@ -4,7 +4,7 @@ let uuid = require('uuid');
 let multer = require('multer');
 let multerS3 = require('multer-s3');
 
-AWS.config.loadFromPath("/home/szczepan/dev_projects/AA/GraphQL/disinterest/config/aws_credentials.json");
+AWS.config.loadFromPath('/Users/michaelwarner/Desktop/GraphQL Docker Apollo Project Repo /disinterest/config/aws_credentials.json');
 
 const s3 = new AWS.S3();
 
