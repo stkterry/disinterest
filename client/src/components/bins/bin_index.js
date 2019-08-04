18177e34a13d6ad72c09bbae6f9b6cabb4389bb8
charles mancuso <charlesmancuso66@yahoo.com>
2019-08-04 11:48:44 -0700

    styling for bin pins

--- client/src/components/bins/bin_index.js
+++ client/src/components/bins/bin_index.js
@@ -6,83 +6,113 @@ const { FETCH_BINS } = Queries;
 
 class BinIndex extends Component {
 
-    randPinLayout() {
-        let layouts = ["layout-1", "layout-2", "layout-3", "layout-4", "layout-5", "layout-6"]
-        let layout = layouts[Math.floor(Math.random()*layouts.length)];
-        return layout;
-    }
-
     render() {
         return ( 
             <div className="bin-index-container">
                 <div className="actual-bin-container">
-                    <div className="layout-1 bin-contents">
-                        <div className="layout1-pic-1">pic1</div>
-                        <div className="layout-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="half-box">pic1</div>
+                            <div className="half-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic3</div>
+                            <div className="half-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="tiny-box">pic5</div>
+                            <div className="big-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
                 </div>
                 <div className="actual-bin-container">
-                    <div className="layout-2 bin-contents">
-                        <div className="bin-pic-1">pic1</div>
-                        <div className="bin-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="half-box">pic1</div>
+                            <div className="half-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic3</div>
+                            <div className="half-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="big-box">pic5</div>
+                            <div className="tiny-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
                 </div>
                 <div className="actual-bin-container">
-                    <div className="layout-3 bin-contents">
-                        <div className="bin-pic-1">pic1</div>
-                        <div className="bin-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="half-box">pic1</div>
+                            <div className="half-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="tiny-box">pic3</div>
+                            <div className="big-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic5</div>
+                            <div className="half-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
                 </div>
                 <div className="actual-bin-container">
-                    <div className="layout-4 bin-contents">
-                        <div className="bin-pic-1">pic1</div>
-                        <div className="bin-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="half-box">pic1</div>
+                            <div className="half-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="big-box">pic3</div>
+                            <div className="tiny-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic5</div>
+                            <div className="half-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
                 </div>
                 <div className="actual-bin-container">
-                    <div className="layout-5 bin-contents">
-                        <div className="bin-pic-1">pic1</div>
-                        <div className="bin-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="tiny-box">pic1</div>
+                            <div className="big-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic3</div>
+                            <div className="half-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic5</div>
+                            <div className="half-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
                 </div>
                 <div className="actual-bin-container">
-                    <div className="layout-6 bin-contents">
-                        <div className="bin-pic-1">pic1</div>
-                        <div className="bin-pic-2">pic2</div>
-                        <div className="bin-pic-3">pic3</div>
-                        <div className="bin-pic-4">pic4</div>
-                        <div className="bin-pic-5">pic5</div>
-                        <div className="bin-pic-6">pic6</div>
+                    <div className="bin-contents">
+                        <div className="bin-box">
+                            <div className="big-box">pic1</div>
+                            <div className="tiny-box">pic2</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic3</div>
+                            <div className="half-box">pic4</div>
+                        </div>
+                        <div className="bin-box">
+                            <div className="half-box">pic5</div>
+                            <div className="half-box">pic6</div>
+                        </div>
                     </div>
                     <div className="bin-name">bin name</div>
                     <div className="number-of-pins"># of pins</div>
