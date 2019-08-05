61063f3968b1e68ae4972a2c8f438ace1ac87c97
Steven K. Terry <stkterry@gmail.com>
2019-08-05 16:29:10 -0400

    heroku bug fixes 9

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const path = require("path");
 if (process.env.NODE_ENV === 'production') {
-  db = require("../config/keys_prod").mongoURI;
+  // db = require("../config/keys_prod").mongoURI;
   app.use(express.static('client/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
