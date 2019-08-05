cdb75b8ef324792cbed51c8d657856f95342769f
Steven K. Terry <stkterry@gmail.com>
2019-08-05 16:03:23 -0400

    heroku bug fixes 5

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY,
+
+  aws_accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  aws_secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_region: process.env.AWS_REGION
 }
--- server/upload_file.js
+++ server/upload_file.js
@@ -4,10 +4,19 @@ let uuid = require('uuid');
 let multer = require('multer');
 let multerS3 = require('multer-s3');
 
-const aws_credentials = require("../config/aws_credentials.json");
-AWS.config.update({ "accessKeyId": aws_credentials.accessKeyId, "secretAccessKey": aws_credentials.secretAccessKey, "region": aws_credentials.region});
 
-// AWS.config.loadFromPath('/Users/michaelwarner/Desktop/GraphQL Docker Apollo Project Repo /disinterest/config/aws_credentials.json');
+let aws_creds;
+if (process.env.NODE_ENV === 'production') {
+  aws_creds = require("../config/keys_prod");
+} else if (process.env.NODE_ENV === 'development') {
+  aws_creds = require("../config/aws_credentials.json");
+}
+AWS.config.update({ 
+  "accessKeyId": aws_creds.aws_accessKeyId, 
+  "secretAccessKey": aws_creds.aws_secretAccessKey, 
+  "region": aws_creds.aws_region 
+});
+
 
 
 const s3 = new AWS.S3();
